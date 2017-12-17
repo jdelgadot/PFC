9a628fbaffea4ab87b7929a5b59c08a749ee29bc
jdelgadot <jadtmat@gmail.com>
2017-12-17 19:28:42 +0000

    Inyectamos el service y añadimos acciones a los botones

--- pra3/src/main/java/pfc/vaadin/pra3/views/LoginView.java
+++ pra3/src/main/java/pfc/vaadin/pra3/views/LoginView.java
@@ -14,9 +14,9 @@ import pfc.vaadin.pra3.editors.LoginEditor;
 public class LoginView extends VerticalLayout implements View {
 
 	
-	
 	@Inject CredentialService service;
-	private LoginEditor editor = new LoginEditor(true, true, false, 4, service);
+	
+	private LoginEditor editor = new LoginEditor(true, true, false, 4);
 	
 	public LoginView() {
 		addComponent(editor);
@@ -24,5 +24,28 @@ public class LoginView extends VerticalLayout implements View {
 		editor.getCreateAccount().addClickListener(e->{
 			getUI().getNavigator().navigateTo("register");
 		});
+		
+		/**
+		 * Registramos un oyente de click al botón que da entrada a la plataforma.
+		 * 
+		 */
+		editor.getLoginButton().addClickListener(e -> {
+			if(editor.getBinder().validate().isOk()) {	
+				//En este punto service es null LoginLayout debe ser manejado por CDI
+				boolean valido = service.isValid(editor.getUser().getValue(), editor.getPass().getValue());
+				if(!valido) {
+					editor.getErrorUser().setVisible(true);
+					editor.getErrorUser().setValue("Usuario o ocntraseña no válida");
+				} else {
+					editor.getErrorUser().setVisible(false);
+					editor.getErrorUser().setValue("");
+				}
+			}
+		});
+		
+		
 	}
+	
+	
+	
 }
